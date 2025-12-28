b1c7ff3a878d524bd575b3f089816e8dccda7f9e
anggathestarboy <anggarathestarboy@gmail.com>
2025-12-28 21:16:11 +0700

    add pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
-createRoot(document.getElementById('root')).render(
-
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <BrowserRouter>
-     <Provider store={store}>
-    <App />
-  </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
 
-
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+      navigator.serviceWorker.register('/sw.js');
+  });
+}
